a2e655a4e2f657ed48c290fe5f3c9975d564056a
daikiyamada32454 <waltz32454@gmail.com>
2020-10-16 18:39:23 +0900

    protospaceの修正３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -10,27 +10,27 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
   end
 
-  def create
-    @prototype = Prototype.new(prototype_params)
-    if @prototype.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-    def show
-      @prototype = Prototype.find(params[:id])
-      @comment = Comment.new
-      @comments = @prototype.comment.includes(:user)
-    end
-
-    def edit 
-      @prototype = Prototype.find(params[:id])
-      unless current_user == @prototype.user
-        redirect_to action: :index
+    def create
+      @prototype = Prototype.new(prototype_params)
+      if @prototype.save
+        redirect_to root_path
+      else
+        render :new
       end
     end
+
+      def show
+        @prototype = Prototype.find(params[:id])
+        @comment = Comment.new
+        @comments = @prototype.comment.includes(:user)
+      end
+
+      def edit 
+        @prototype = Prototype.find(params[:id])
+        unless current_user == @prototype.user
+          redirect_to action: :index
+        end
+      end
 
     def update 
       @prototype = Prototype.find(params[:id])
